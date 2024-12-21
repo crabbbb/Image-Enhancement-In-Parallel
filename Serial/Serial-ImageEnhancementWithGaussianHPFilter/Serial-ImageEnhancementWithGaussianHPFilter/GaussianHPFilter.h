e65a361cd60aee4cff806bb66d1c8b64e3350448
YUHER\yuher <yuherlim123@gmail.com>
2024-12-21 21:14:18 +0800

    updated implementation of gaussian filter

--- Serial/Serial-ImageEnhancementWithGaussianHPFilter/Serial-ImageEnhancementWithGaussianHPFilter/GaussianHPFilter.h
+++ Serial/Serial-ImageEnhancementWithGaussianHPFilter/Serial-ImageEnhancementWithGaussianHPFilter/GaussianHPFilter.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <complex>
 
+using namespace std;
+
 // Function declarations
 double computeHighPassValue(int u, int v, int height, int width, double cutoff_frequency);
-void gaussianHighPassFilter(const std::complex<double>* F_shifted, std::complex<double>* G, int width, int height, double cutoff_frequency);
+void gaussianHighPassFilter(
+    const complex<double>** F_shifted, // 2D input array
+    complex<double>** G,              // 2D output array
+    int width,
+    int height,
+    double cutoff_frequency);
 bool testGaussianHighPassFilter();
