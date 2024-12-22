38668159747de12486b080214aad5ab425f71b20
YUHER\yuher <yuherlim123@gmail.com>
2024-12-22 22:27:50 +0800

    finally fourier transform cuda done

--- CUDA/CUDA-ImageEnhancementWithGaussianHPFilter/CUDA-ImageEnhancementWithGaussianHPFilter/CUDA-FastFourierTransform.cuh
+++ CUDA/CUDA-ImageEnhancementWithGaussianHPFilter/CUDA-ImageEnhancementWithGaussianHPFilter/CUDA-FastFourierTransform.cuh
@@ -6,15 +6,21 @@
 
 #define M_PI 3.14159265358979323846
 
+int bitReverseIndex(int x, int log2N);
+
+void bitReverseReorder(cuDoubleComplex* data, int N);
+
 // CUDA Kernel for Cooley-Tukey FFT
 __global__ void FFT1DKernel(cuDoubleComplex* data, int size, int step);
 
 // Host Function for Parallelized 1D FFT
-void FFT1DParallel(uint8_t* h_input, cuDoubleComplex* h_output, int size);
+void FFT1DParallel(cuDoubleComplex* h_input, cuDoubleComplex* h_output, int size);
 
 // 2D FFT Implementation
-cuDoubleComplex** FFT2D(uint8_t* grayscaleImage, int width, int height);
+cuDoubleComplex** FFT2DParallel(const uint8_t* grayscaleImage, int width, int height);
 
 // Test Function for 2D FFT
-bool testFFT2D();
+bool testFFT2DParallel();
+
+cuDoubleComplex* convertToComplex(uint8_t* grayscale, int size);
 
