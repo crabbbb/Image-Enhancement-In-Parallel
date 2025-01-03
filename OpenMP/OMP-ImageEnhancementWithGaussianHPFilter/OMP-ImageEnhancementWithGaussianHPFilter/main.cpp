5412d55b98fcd90d301aeb4a3c04c9c41b23fb16
crabbbb <xuezhanixi666@gmail.com>
2025-01-04 03:42:56 +0800

    omp argument feature done

--- OpenMP/OMP-ImageEnhancementWithGaussianHPFilter/OMP-ImageEnhancementWithGaussianHPFilter/main.cpp
+++ OpenMP/OMP-ImageEnhancementWithGaussianHPFilter/OMP-ImageEnhancementWithGaussianHPFilter/main.cpp
@@ -16,7 +16,7 @@ const double CUTOFF_FREQUENCY = 100;
 const double ALPHA = 1.0;
 
 // all the processing done here 
-cv::Mat startProcessing(cv::Mat& in_img, string imName, int cutoff_frequency, double alpha) {
+cv::Mat startProcessing(cv::Mat& in_img, string imName, int cutoff_frequency, double alpha, bool isArgument = true) {
 
     // get width and height 
     int width = in_img.cols;
@@ -82,25 +82,42 @@ cv::Mat startProcessing(cv::Mat& in_img, string imName, int cutoff_frequency, do
 
     cout << "Total duration time used for OpenMP is " << duration << "ms " << endl;
 
-    //storeDataIntoFile(duration, "omp", imName);
+    // only when is not argument then can write the file 
+    if (!isArgument) {
+        storeDataIntoFile(duration, "cuda", imName);
+    }
 
     // save image 
     // ------------ this path is for using python execute ------------------------------
-    //cv::imwrite("resource/result/omp/" + imName + "_gray.jpg", fromUint8ToMat(channelImage, width, height));
-    //cv::imwrite("resource/result/omp/" + imName + "_fft.jpg", fromUint8ToMat(fftImage, width, height));
-    //cv::imwrite("resource/result/omp/" + imName + "_gaussian.jpg", fromUint8ToMat(gaussianImage, width, height));
+    cv::imwrite("resource/result/omp/" + imName + "_gray.jpg", fromUint8ToMat(channelImage, width, height));
+    cv::imwrite("resource/result/omp/" + imName + "_fft.jpg", fromUint8ToMat(fftImage, width, height));
+    cv::imwrite("resource/result/omp/" + imName + "_gaussian.jpg", fromUint8ToMat(gaussianImage, width, height));
 
     // ------------ this path is for using visual studio ------------------------------
-    cv::imwrite("../../../resource/result/omp/" + imName + "_gray.jpg", fromUint8ToMat(channelImage, width, height));
-    cv::imwrite("../../../resource/result/omp/" + imName + "_fft.jpg", fromUint8ToMat(fftImage, width, height));
-    cv::imwrite("../../../resource/result/omp/" + imName + "_gaussian.jpg", fromUint8ToMat(gaussianImage, width, height));
+    //cv::imwrite("../../../resource/result/omp/" + imName + "_gray.jpg", fromUint8ToMat(channelImage, width, height));
+    //cv::imwrite("../../../resource/result/omp/" + imName + "_fft.jpg", fromUint8ToMat(fftImage, width, height));
+    //cv::imwrite("../../../resource/result/omp/" + imName + "_gaussian.jpg", fromUint8ToMat(gaussianImage, width, height));
 
     // convert back
     cv::Mat out_img = fromUint8ToMat(ifftImage, width, height);
-    //cv::imwrite("resource/result/omp/" + imName + "_ifft.jpg", out_img);
-    cv::imwrite("../../../resource/result/omp/" + imName + "_ifft.jpg", out_img);
+    cv::imwrite("resource/result/omp/" + imName + "_ifft.jpg", out_img);
+    //cv::imwrite("../../../resource/result/omp/" + imName + "_ifft.jpg", out_img);
 
     return out_img;
+}
+
+bool isImageFile(const string& file_path) {
+    // List of valid image extensions
+    const vector<string> valid_extensions = { ".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".gif" };
+
+    // Get file extension
+    string extension = filesystem::path(file_path).extension().string();
+
+    // Convert to lowercase for case-insensitive comparison
+    transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
+
+    // Check if extension is in the list of valid image extensions
+    return find(valid_extensions.begin(), valid_extensions.end(), extension) != valid_extensions.end();
 }
 
 // will process multiple color channel with the passing argument 
@@ -137,23 +154,22 @@ void processRGB(int cutoff_frequency, double alpha, string file_path)
 
         cv::Mat processedChannel = startProcessing(bgrChannels[c], imName, cutoff_frequency, alpha);
 
-        cv::imwrite("../../../resource/result/omp/" + imName + "channel_" + color + ".jpg", processedChannel);
+        cv::imwrite("resource/result/omp/" + imName + "channel_" + color + ".jpg", processedChannel);
 
         bgrChannels[c] = processedChannel;
     }
 
     cv::Mat mergedResult;
     cv::merge(bgrChannels, mergedResult);
-    cv::imwrite("../../../resource/result/omp/" + imName + "merged_result.jpg", mergedResult);
+    cv::imwrite("resource/result/omp/" + imName + "merged_result.jpg", mergedResult);
 }
 
-void processSingleImChannel()
+void processGreyscale()
 {
-    //string image[] = { "doggo.jpg", "cameragirl.jpeg", "lena.jpeg", "wolf.jpg" };
-    string image[] = { "cameragirl.jpeg" };
+    string image[] = { "doggo.jpg", "cameragirl.jpeg", "lena.jpeg", "wolf.jpg" };
 
-    //string basePath = "resource/raw/";
-    string basePath = "../../../resource/raw/";
+    string basePath = "resource/raw/";
+    //string basePath = "../../../resource/raw/";
 
     cv::Mat rgbImage;
     cv::Mat out;
@@ -179,12 +195,79 @@ void processSingleImChannel()
     //cv::waitKey(0);
 }
 
+void processArguments(int argc, char* argv[]) {
+    if (argc < 5) {
+        cout << "Insufficient number of arguments." << endl;
+        cout << "Usage: exe_file_to_execute -single [cutoff frequency] [alpha] [image file path]" << endl;
+    }
+
+    int cutoff_frequency;
+    double alpha;
+    string file_path = argv[4];
+    bool valid_cutoff_frequency = false;
+    bool valid_alpha = false;
+    bool valid_file = false;
+    cout << "cutoff_frequency" << "\n" << endl;
+
+    try {
+        cutoff_frequency = stoi(argv[2]);
+        if (cutoff_frequency < 0 || cutoff_frequency > 255) {
+            cout << "Cutoff frequency must be an integer between 0 and 255." << endl;
+        }
+        else {
+            cout << "Cutoff frequency is valid: " << cutoff_frequency << endl;
+            valid_cutoff_frequency = true;
+        }
+    }
+    catch (const exception& e) {
+        cout << "Invalid cutoff frequency. Please provide an integer." << endl;
+    }
+
+    try {
+        alpha = stod(argv[3]);
+        if (alpha > 0) {
+            cout << "Alpha value is valid for sharpening: " << alpha << endl;
+            valid_alpha = true;
+        }
+        else {
+            cout << "Alpha value must be greater than 0 for sharpening." << endl;
+        }
+    }
+    catch (const exception& e) {
+        cout << "Invalid alpha value. Please provide a valid floating-point number." << endl;
+    }
+
+    // File path validation
+    if (filesystem::exists(file_path)) {
+        if (isImageFile(file_path)) {
+            cout << "File path is valid, and it is an image: " << file_path << endl;
+            valid_file = true;
+        }
+        else {
+            cout << "Invalid file type. Please provide an image file (e.g. .jpg, .jpeg, .png, .bmp, .tiff, .gif)." << endl;
+        }
+    }
+    else {
+        cout << "Invalid file path. The specified file does not exist: " << file_path << endl;
+    }
+
+    if (valid_cutoff_frequency && valid_alpha && valid_file) {
+        cout << "All inputs are valid. Proceeding with processing..." << endl;
+        processRGB(cutoff_frequency, alpha, file_path);
+    }
+    else {
+        cout << "Usage: exe_file_to_execute -single [cutoff frequency] [alpha] [image file path]" << endl;
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    
-    
+    if (argc > 1 && strcmp(argv[1], "-single") == 0) {
+        processArguments(argc, argv);
+        return 0;
+    }
 
-    
+    processGreyscale();
 
     return 0;
 }
